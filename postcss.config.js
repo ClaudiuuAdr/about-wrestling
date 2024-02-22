71b1a0f6cc12947e9111b1d3644283ba98a0e703
Claudiu Adrian <cocheciclaudiu@gmail.com>
2024-02-22 14:06:55 +0200

    postcss update

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,9 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-}
+};
